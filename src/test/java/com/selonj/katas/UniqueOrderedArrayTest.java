ae9ab4b315e4cb1d6041da477a8a59ab91b98cf6
get-started-tdd <32414154@qq.com>
2016-02-25 00:26:05 +0800

    create test class => UniqueOrderedArrayTest

---
+++ src/test/java/com/selonj/katas/UniqueOrderedArrayTest.java
@@ -0,0 +1,15 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by L.x on 16-2-25.
+ */
+public class UniqueOrderedArrayTest {
+    @Test
+    public void nothing() throws Exception {
+        fail("fail fast");
+    }
+}
