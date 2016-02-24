fa01e2546be0a4bdffc8d4aa2ad305731d24b40d
get-started-tdd <32414154@qq.com>
2016-02-25 00:27:47 +0800

    write the first test

--- src/test/java/com/selonj/katas/UniqueOrderedArrayTest.java
+++ src/test/java/com/selonj/katas/UniqueOrderedArrayTest.java
@@ -2,14 +2,14 @@ package com.selonj.katas;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by L.x on 16-2-25.
  */
 public class UniqueOrderedArrayTest {
     @Test
-    public void nothing() throws Exception {
-        fail("fail fast");
+    public void emptyArray() throws Exception {
+        assertThat(unique(new int[0]), returns(new int[0]));
     }
 }
