8727bffe6bd640c3a11a850c500446b2a6b2e138
get-started-tdd <32414154@qq.com>
2016-02-25 00:45:36 +0800

    refactoring => remove another if statement because they are duplicated

--- src/test/java/com/selonj/katas/UniqueOrderedArrayTest.java
+++ src/test/java/com/selonj/katas/UniqueOrderedArrayTest.java
@@ -40,7 +40,6 @@ public class UniqueOrderedArrayTest {
         int n = 0;
         for (int i = 0; array.length > i; i++) {
             if (i > 0 && array[i] == results[n - 1]) ;
-            else if (i > 0 && array[i] == results[n - 1]) ;
             else
                 results[n++] = array[i];
         }
