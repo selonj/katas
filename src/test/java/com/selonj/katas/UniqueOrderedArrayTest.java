2c210a192e56e4d879ec692287900bb769191ce6
get-started-tdd <32414154@qq.com>
2016-02-25 00:28:48 +0800

    make test compile

--- src/test/java/com/selonj/katas/UniqueOrderedArrayTest.java
+++ src/test/java/com/selonj/katas/UniqueOrderedArrayTest.java
@@ -1,7 +1,9 @@
 package com.selonj.katas;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -11,5 +13,13 @@ public class UniqueOrderedArrayTest {
     @Test
     public void emptyArray() throws Exception {
         assertThat(unique(new int[0]), returns(new int[0]));
+    }
+
+    private int[] unique(int[] array) {
+        return null;
+    }
+
+    private Matcher<? super int[]> returns(int[] expected) {
+        return equalTo(expected);
     }
 }
