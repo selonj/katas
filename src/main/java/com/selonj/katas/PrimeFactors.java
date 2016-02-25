14e8ce2e1fc3518dcae9f5b1c9ac710e7faff127
get-started-tdd <32414154@qq.com>
2016-02-25 15:27:56 +0800

    make the test pass by add mod operation

--- src/main/java/com/selonj/katas/PrimeFactors.java
+++ src/main/java/com/selonj/katas/PrimeFactors.java
@@ -10,7 +10,13 @@ public class PrimeFactors {
     public static List<Integer> of(int n) {
         ArrayList<Integer> primes = new ArrayList<>();
         if (n > 1) {
-            primes.add(n);
+            if (n % 2 == 0) {
+                primes.add(2);
+                n /= 2;
+            }
+            if (n > 1) {
+                primes.add(n);
+            }
         }
         return primes;
     }
