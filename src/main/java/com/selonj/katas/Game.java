073bc7eb7a80b3ea0430cfd54ef3c0c9686db579
get-started-tdd <32414154@qq.com>
2016-02-26 15:52:41 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/Game.java
@@ -0,0 +1,14 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-2-26.
+ */
+public class Game {
+    public void roll(int pins) {
+
+    }
+
+    public int score() {
+        return -1;
+    }
+}
