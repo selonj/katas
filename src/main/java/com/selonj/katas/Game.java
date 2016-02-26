14e56f9779f30f967f2a12c35483e89cbdb6a4aa
get-started-tdd <32414154@qq.com>
2016-02-26 16:14:33 +0800

    refactoring => extract constant to describe domain

--- src/main/java/com/selonj/katas/Game.java
+++ src/main/java/com/selonj/katas/Game.java
@@ -4,7 +4,9 @@ package com.selonj.katas;
  * Created by L.x on 16-2-26.
  */
 public class Game {
-    public static final int ROLLS_COUNT = 21;
+    public static final int FRAMES = 10;
+    public static final int ROLLS_COUNT = 2 * FRAMES + 1;
+
     private int[] rolls = new int[ROLLS_COUNT];
     private int size;
 
@@ -15,7 +17,7 @@ public class Game {
     public int score() {
         int score = 0;
         int i = 0;
-        for (int frame = 0; frame < 10; frame++) {
+        for (int frame = 0; frame < FRAMES; frame++) {
             score += rolls[i] + rolls[i + 1];
             i += 2;
         }
