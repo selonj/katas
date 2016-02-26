8f5d70616fdd0244cbc1530ab7b9aeac3a577e08
get-started-tdd <32414154@qq.com>
2016-02-26 15:51:17 +0800

    write the 1st test

---
+++ src/test/java/com/selonj/katas/BowlingGameTest.java
@@ -0,0 +1,21 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-2-26.
+ */
+public class BowlingGameTest {
+    @Test
+    public void noPinsKnockedDownInAllRolls() throws Exception {
+        Game game = new Game();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+
+        assertThat(game.score(), equalTo(0));
+    }
+}
