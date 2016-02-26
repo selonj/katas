874cc96d41418391d0449c12fb9ed021c8052191
get-started-tdd <32414154@qq.com>
2016-02-26 15:56:54 +0800

    refactoring => remove game creation duplication

--- src/test/java/com/selonj/katas/BowlingGameTest.java
+++ src/test/java/com/selonj/katas/BowlingGameTest.java
@@ -1,5 +1,6 @@
 package com.selonj.katas;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -9,9 +10,16 @@ import static org.junit.Assert.assertThat;
  * Created by L.x on 16-2-26.
  */
 public class BowlingGameTest {
+
+    private Game game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+    }
+
     @Test
     public void noPinsKnockedDownInAllRolls() throws Exception {
-        Game game = new Game();
         for (int i = 0; i < 20; i++) {
             game.roll(0);
         }
@@ -21,7 +29,6 @@ public class BowlingGameTest {
 
     @Test
     public void onePinKnockedDownInAllRolls() throws Exception {
-        Game game = new Game();
         for (int i = 0; i < 20; i++) {
             game.roll(1);
         }
