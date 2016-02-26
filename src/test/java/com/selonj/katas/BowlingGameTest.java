a016147618a92ad545975464ed58b1b6be0d4878
get-started-tdd <32414154@qq.com>
2016-02-26 15:58:35 +0800

    refactoring => remove roll loop duplication

--- src/test/java/com/selonj/katas/BowlingGameTest.java
+++ src/test/java/com/selonj/katas/BowlingGameTest.java
@@ -20,18 +20,20 @@ public class BowlingGameTest {
 
     @Test
     public void noPinsKnockedDownInAllRolls() throws Exception {
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        rollMany(20, 0);
 
         assertThat(game.score(), equalTo(0));
     }
 
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            game.roll(pins);
+        }
+    }
+
     @Test
     public void onePinKnockedDownInAllRolls() throws Exception {
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        rollMany(20, 1);
 
         assertThat(game.score(), equalTo(20));
     }
