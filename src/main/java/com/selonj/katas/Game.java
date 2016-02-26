9eeaa07cc83e38be28088ba9e3b543c96c945551
get-started-tdd <32414154@qq.com>
2016-02-26 16:07:17 +0800

    refactoring => calculate score in Game.score() method

--- src/main/java/com/selonj/katas/Game.java
+++ src/main/java/com/selonj/katas/Game.java
@@ -5,16 +5,18 @@ package com.selonj.katas;
  */
 public class Game {
     public static final int ROLLS_COUNT = 21;
-    private int score;
     private int[] rolls = new int[ROLLS_COUNT];
     private int size;
 
     public void roll(int pins) {
-        score += pins;
         rolls[size++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++) {
+            score += rolls[i];
+        }
         return score;
     }
 }
