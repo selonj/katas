51ddf225cfc53bb264999d08fd7e5b61b9059485
get-started-tdd <32414154@qq.com>
2016-02-27 21:01:34 +0800

    make the test pass by linear recursive search of first char

--- src/main/java/com/selonj/katas/Strings.java
+++ src/main/java/com/selonj/katas/Strings.java
@@ -12,12 +12,14 @@ public class Strings {
             return true;
         }
         int pos = -1;
-        pos = s2.indexOf(s1.charAt(0), pos + 1);
-        if (pos == -1) {
-            return false;
-        }
-        if (s1.equals(s2.substring(pos) + s2.substring(0, pos))) {
-            return true;
+        while (true) {
+            pos = s2.indexOf(s1.charAt(0), pos + 1);
+            if (pos == -1) {
+                break;
+            }
+            if (s1.equals(s2.substring(pos) + s2.substring(0, pos))) {
+                return true;
+            }
         }
         return false;
     }
