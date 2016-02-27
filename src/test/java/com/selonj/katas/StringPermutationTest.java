3aee7205d7f946b3b57f9e0580dd39d6f3e3e7e5
get-started-tdd <32414154@qq.com>
2016-02-27 19:25:38 +0800

    write the 1st test

---
+++ src/test/java/com/selonj/katas/StringPermutationTest.java
@@ -0,0 +1,17 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-2-27.
+ */
+public class StringPermutationTest {
+
+    @Test
+    public void emptyStrings() throws Exception {
+        assertThat(Strings.isPermutationOf("",""),is(true));
+    }
+}
