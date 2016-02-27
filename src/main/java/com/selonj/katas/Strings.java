2ca18375022feaebfdb605170d78862a768716c2
get-started-tdd <32414154@qq.com>
2016-02-27 20:47:53 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/Strings.java
@@ -0,0 +1,10 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-2-27.
+ */
+public class Strings {
+    public static boolean isPermutationOf(String s1, String s2) {
+        return false;
+    }
+}
