03b954b1954eaade121af868b778bb17751638c7
get-started-tdd <32414154@qq.com>
2016-02-28 20:06:00 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/Version.java
@@ -0,0 +1,15 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-2-28.
+ */
+public class Version implements Comparable<Version> {
+    public Version(String version) {
+
+    }
+
+    @Override
+    public int compareTo(Version that) {
+        return 0;
+    }
+}
--- src/test/java/com/selonj/katas/VersionComparisonTest.java
+++ src/test/java/com/selonj/katas/VersionComparisonTest.java
@@ -11,6 +11,10 @@ import static org.junit.Assert.assertThat;
 public class VersionComparisonTest {
     @Test
     public void withoutMinorVersions() throws Exception {
-        assertThat(version("1").compare(version("0")), equalTo(1));
+        assertThat(version("1").compareTo(version("0")), equalTo(1));
+    }
+
+    private Version version(String version) {
+        return new Version(version);
     }
 }
