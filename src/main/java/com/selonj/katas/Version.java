d8b75048a17979e40467fbc232bac27746d2602c
get-started-tdd <32414154@qq.com>
2016-02-28 20:13:22 +0800

    make the test pass by compare two version strings

--- src/main/java/com/selonj/katas/Version.java
+++ src/main/java/com/selonj/katas/Version.java
@@ -4,12 +4,15 @@ package com.selonj.katas;
  * Created by L.x on 16-2-28.
  */
 public class Version implements Comparable<Version> {
+    private String version;
+
     public Version(String version) {
 
+        this.version = version;
     }
 
     @Override
     public int compareTo(Version that) {
-        return 1;
+        return version.compareTo(that.version);
     }
 }
