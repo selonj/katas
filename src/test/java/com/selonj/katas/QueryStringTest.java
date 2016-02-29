9c8dcfa2c154ed62da8c0acbc330f245c06de10c
get-started-tdd <32414154@qq.com>
2016-02-29 21:03:41 +0800

    write the 6th test

--- src/test/java/com/selonj/katas/QueryStringTest.java
+++ src/test/java/com/selonj/katas/QueryStringTest.java
@@ -2,6 +2,7 @@ package com.selonj.katas;
 
 import org.junit.Test;
 
+import static com.selonj.katas.QueryString.param;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -11,19 +12,23 @@ import static org.junit.Assert.assertThat;
 public class QueryStringTest {
     @Test
     public void singleParameter() throws Exception {
-        assertThat(QueryString.within(QueryString.param("key", "value")).toString(), equalTo("?key=value"));
-        assertThat(QueryString.within(QueryString.param("foo", "bar")).toString(), equalTo("?foo=bar"));
+        assertThat(QueryString.within(param("key", "value")).toString(), equalTo("?key=value"));
+        assertThat(QueryString.within(param("foo", "bar")).toString(), equalTo("?foo=bar"));
     }
 
     @Test
     public void multiParameters() throws Exception {
-        assertThat(QueryString.within(QueryString.param("key", "value"), QueryString.param("foo", "bar")).toString(), equalTo("?key=value&foo=bar"));
-        assertThat(QueryString.within(QueryString.param("a", "b"), QueryString.param("c", "d"), QueryString.param("e", "f")).toString(), equalTo("?a=b&c=d&e=f"));
+        assertThat(QueryString.within(param("key", "value"), param("foo", "bar")).toString(), equalTo("?key=value&foo=bar"));
+        assertThat(QueryString.within(param("a", "b"), param("c", "d"), param("e", "f")).toString(), equalTo("?a=b&c=d&e=f"));
     }
 
     @Test
     public void withoutAnyParameters() throws Exception {
-        assertThat(QueryString.withNothing().toString(),equalTo(""));
+        assertThat(QueryString.withNothing().toString(), equalTo(""));
     }
 
+    @Test
+    public void parameterWithNoValue() throws Exception {
+        assertThat(QueryString.within(param("key", null)).toString(), equalTo("?key="));
+    }
 }
