f374832962075759589cb36a952e33184c86a55b
get-started-tdd <32414154@qq.com>
2016-02-29 20:23:43 +0800

    write the 1st test

---
+++ src/test/java/com/selonj/katas/QueryStringTest.java
@@ -0,0 +1,16 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-2-29.
+ */
+public class QueryStringTest {
+    @Test
+    public void singleParameter() throws Exception {
+        assertThat(QueryString.within(param("key", "value")).toString(), equalTo("?key=value"));
+    }
+}
