96761ed28794a0b93d7cd40f609ad83295b658ad
get-started-tdd <32414154@qq.com>
2016-03-01 21:35:48 +0800

    adjust the code to start refacotring

--- src/main/java/com/selonj/katas/BerlinClock.java
+++ src/main/java/com/selonj/katas/BerlinClock.java
@@ -4,6 +4,7 @@ package com.selonj.katas;
  * Created by L.x on 16-3-1.
  */
 public class BerlinClock {
+    public static final String LITED_LIGHT = "RRRR";
     private final int hours;
 
     public BerlinClock(String time) {
@@ -24,11 +25,11 @@ public class BerlinClock {
 
     public String getHoursOfRow1() {
         if (hours / 5 == 2) {
-            return "RR" + "00";
+            return LITED_LIGHT.substring(0, 2) + "00";
         }
         if (hours / 5 == 1) {
-            return "R" + "000";
+            return LITED_LIGHT.substring(0, 1) + "000";
         }
-        return "" + "0000";
+        return LITED_LIGHT.substring(0, 0) + "0000";
     }
 }
