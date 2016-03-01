b90eda3cae27b6081a3174c29af77ecfa54e4120
get-started-tdd <32414154@qq.com>
2016-03-01 21:02:13 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/BerlinClock.java
@@ -0,0 +1,14 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-3-1.
+ */
+public class BerlinClock {
+    public static BerlinClock at(String time) {
+        return null;
+    }
+
+    public String display() {
+        return null;
+    }
+}
