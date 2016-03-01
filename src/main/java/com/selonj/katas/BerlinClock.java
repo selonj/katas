15569672a24f77ab63ce87e1fdf31f9626b3fcdd
get-started-tdd <32414154@qq.com>
2016-03-01 22:30:05 +0800

    refactoring => replace magic numbers with constants

--- src/main/java/com/selonj/katas/BerlinClock.java
+++ src/main/java/com/selonj/katas/BerlinClock.java
@@ -4,9 +4,26 @@ package com.selonj.katas;
  * Created by L.x on 16-3-1.
  */
 public class BerlinClock {
+    private static final String[] LIT_LIGHTS = new String[]{
+            "RRRR",
+            "RRRR",
+            "YYRYYRYYRYY",
+            "YYYY"
+    };
+    private static final String[] UNLIT_LIGHTS = new String[]{
+            "0000",
+            "0000",
+            "00000000000",
+            "0000"
+    };
+    private static final int ROW_OF_HOURS1 = 0;
+    private static final int ROW_OF_HOURS2 = 1;
+    private static final int ROW_OF_MINUTES1 = 2;
+    private static final int ROW_OF_MINUTES2 = 3;
+
+    private static final String TIME_SEP = ":";
     private static final int HOURS_POS = 0;
     private static final int MINUTES_POS = 1;
-    private static final String TIME_SEP = ":";
     private final int hours;
     private final int minutes;
 
@@ -27,33 +44,21 @@ public class BerlinClock {
                 getMinutesOfRow2();
     }
 
-    private static final String[] LIT_LIGHTS = new String[]{
-            "RRRR",
-            "RRRR",
-            "YYRYYRYYRYY",
-            "YYYY"
-    };
-    private static final String[] UNLIT_LIGHTS = new String[]{
-            "0000",
-            "0000",
-            "00000000000",
-            "0000"
-    };
 
     public String getHoursOfRow1() {
-        return display(0, hours / 5);
+        return display(ROW_OF_HOURS1, hours / 5);
     }
 
     public String getHoursOfRow2() {
-        return display(1, hours % 5);
-    }
-
-    public String getMinutesOfRow2() {
-        return display(3, minutes % 5);
+        return display(ROW_OF_HOURS2, hours % 5);
     }
 
     public String getMinutesOfRow1() {
-        return display(2, minutes / 5);
+        return display(ROW_OF_MINUTES1, minutes / 5);
+    }
+
+    public String getMinutesOfRow2() {
+        return display(ROW_OF_MINUTES2, minutes % 5);
     }
 
     private String display(int row, int numberOfLitLights) {
