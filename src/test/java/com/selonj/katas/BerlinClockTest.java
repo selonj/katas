10d2298aed19b4d87061344bc09757d86b0361e0
get-started-tdd <32414154@qq.com>
2016-03-01 21:00:50 +0800

    write the 1st test

---
+++ src/test/java/com/selonj/katas/BerlinClockTest.java
@@ -0,0 +1,21 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-3-1.
+ */
+public class BerlinClockTest {
+    @Test
+    public void displayTime() throws Exception {
+        assertThat(BerlinClock.at("00:00").display(), equalTo(
+                "0000" +
+                "0000" +
+                "00000000000" +
+                "0000"
+        ));
+    }
+}
