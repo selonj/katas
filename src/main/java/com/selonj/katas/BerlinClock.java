9058612870f3ef755565e42ef86f514485488ce1
get-started-tdd <32414154@qq.com>
2016-03-01 22:17:26 +0800

    inline code to start refactoring

--- src/main/java/com/selonj/katas/BerlinClock.java
+++ src/main/java/com/selonj/katas/BerlinClock.java
@@ -4,8 +4,6 @@ package com.selonj.katas;
  * Created by L.x on 16-3-1.
  */
 public class BerlinClock {
-    public static final String LIT_LIGHT_OF_HOURS = "RRRR";
-    public static final String LIT_LIGHT_OF_MINUTES2 = "YYYY";
     private static final int HOURS_POS = 0;
     private static final int MINUTES_POS = 1;
     private static final String TIME_SEP = ":";
@@ -30,38 +28,23 @@ public class BerlinClock {
     }
 
     public String getHoursOfRow1() {
-        return litLightsOfHours(hours / 5) + unlitLights(hours / 5);
-    }
-
-    private String unlitLights(int numberOfLintLights) {
-        return "0000".substring(numberOfLintLights);
-    }
-
-    private String litLightsOfHours(int n) {
-        return LIT_LIGHT_OF_HOURS.substring(0, n);
+        int numberOfLitLights = hours / 5;
+        return "RRRR".substring(0, numberOfLitLights) + "0000".substring(numberOfLitLights);
     }
 
     public String getHoursOfRow2() {
-        return litLightsOfHours(hours % 5) + unlitLights(hours % 5);
+        int numberOfLitLights = hours % 5;
+        return "RRRR".substring(0, numberOfLitLights) + "0000".substring(numberOfLitLights);
     }
 
     public String getMinutesOfRow2() {
-        return litLightsOfMinutes2(minutes % 5) + unlitLights(minutes % 5);
-    }
-
-    private String litLightsOfMinutes2(int numberOfLitLights) {
-        return LIT_LIGHT_OF_MINUTES2.substring(0, numberOfLitLights);
+        int numberOfLitLights = minutes % 5;
+        return "YYYY".substring(0, numberOfLitLights) + "0000".substring(numberOfLitLights);
     }
 
     public String getMinutesOfRow1() {
-        return litLightsOfMinutes1(minutes / 5) + unlitLights2(minutes / 5);
+        int numberOfLitLights = minutes / 5;
+        return "YYRYYRYYRYY".substring(0, numberOfLitLights) + "00000000000".substring(numberOfLitLights);
     }
 
-    private String litLightsOfMinutes1(int n) {
-        return "YYRYYRYYRYY".substring(0, n);
-    }
-
-    private String unlitLights2(int numberOfLitLights) {
-        return "00000000000".substring(numberOfLitLights);
-    }
 }
