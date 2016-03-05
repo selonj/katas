2c0e0088c8a745170bf51d3c369e887f5b157dd1
get-started-tdd <32414154@qq.com>
2016-03-05 23:07:36 +0800

    write the 1st test

---
+++ src/test/java/com/selonj/katas/FuzzBuzzTest.java
@@ -0,0 +1,17 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-3-5.
+ */
+public class FuzzBuzzTest {
+    @Test
+    public void number() throws Exception {
+        assertThat(FuzzBuzz.of(1),equalTo("1"));
+
+    }
+}
