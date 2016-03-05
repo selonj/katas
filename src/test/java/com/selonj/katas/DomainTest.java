4a8b812b873e6c31cc96cf3092c94e387e887dce
zhangyong <vikit_zhang@163.com>
2016-03-05 12:30:14 +0800

    write the 1st test

---
+++ src/test/java/com/selonj/katas/DomainTest.java
@@ -0,0 +1,16 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Administrator on 2016-03-05.
+ */
+public class DomainTest {
+    @Test
+    public void domainOnly() throws Exception {
+        assertThat(Domain.from("google.com").getName(),equalTo("google.com"));
+    }
+}
