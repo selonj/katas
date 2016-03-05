eae8c902baefa6b3e5878bf8059b2ddc5e723fdb
get-started-tdd <32414154@qq.com>
2016-03-05 23:34:10 +0800

    introduce parameter `mod`

--- src/main/java/com/selonj/katas/FuzzBuzz.java
+++ src/main/java/com/selonj/katas/FuzzBuzz.java
@@ -4,12 +4,12 @@ package com.selonj.katas;
  * Created by L.x on 16-3-5.
  */
 public enum FuzzBuzz {
-    Fuzz, Buzz;
+    Fuzz(3), Buzz(5);
 
     private final int mod;
 
-    FuzzBuzz() {
-        mod = 3;
+    FuzzBuzz(int mod) {
+        this.mod = mod;
     }
 
     public boolean test(int n) {
