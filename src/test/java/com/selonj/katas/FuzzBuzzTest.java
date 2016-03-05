cbc3e5be6b3194d2fa9f9caada4a539f68c48ac9
get-started-tdd <32414154@qq.com>
2016-03-05 23:13:22 +0800

    write the 5th test

--- src/test/java/com/selonj/katas/FuzzBuzzTest.java
+++ src/test/java/com/selonj/katas/FuzzBuzzTest.java
@@ -20,4 +20,10 @@ public class FuzzBuzzTest {
         assertThat(FuzzBuzz.of(3), equalTo("Fuzz"));
         assertThat(FuzzBuzz.of(6), equalTo("Fuzz"));
     }
+
+    @Test
+    public void buzz() throws Exception {
+        assertThat(FuzzBuzz.of(5), equalTo("Buzz"));
+        assertThat(FuzzBuzz.of(10), equalTo("Buzz"));
+    }
 }
