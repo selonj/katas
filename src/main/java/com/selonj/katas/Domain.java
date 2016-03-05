c3429a3e946abcc4c7a43d409be3f9effae01e57
zhangyong <vikit_zhang@163.com>
2016-03-05 12:32:38 +0800

    make the test pass by return url directly

--- src/main/java/com/selonj/katas/Domain.java
+++ src/main/java/com/selonj/katas/Domain.java
@@ -5,11 +5,17 @@ package com.selonj.katas;
  * Created by Administrator on 2016-03-05.
  */
 public class Domain {
+    private String url;
+
+    public Domain(String url) {
+        this.url = url;
+    }
+
     public static Domain from(String url) {
-        return new Domain();
+        return new Domain(url);
     }
 
     public String getName() {
-        return "google.com";
+        return url;
     }
 }
