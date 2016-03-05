44a5cf16d5dd64605b84f34f9e3fe77dd746878d
zhangyong <vikit_zhang@163.com>
2016-03-05 12:30:50 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/Domain.java
@@ -0,0 +1,15 @@
+package com.selonj.katas;
+
+
+/**
+ * Created by Administrator on 2016-03-05.
+ */
+public class Domain {
+    public static Domain from(String url) {
+        return null;
+    }
+
+    public String getName() {
+        return null;
+    }
+}
