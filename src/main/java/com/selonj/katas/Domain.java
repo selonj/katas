d5f7641b3d3acf981ebf793d4d2e837e9f54b3e7
zhangyong <vikit_zhang@163.com>
2016-03-05 13:13:01 +0800

    make the test pass by truncate https://

--- src/main/java/com/selonj/katas/Domain.java
+++ src/main/java/com/selonj/katas/Domain.java
@@ -8,9 +8,17 @@ public class Domain {
     private String hostName;
 
     public Domain(String url) {
-        url = url.startsWith("http://") ? url.substring(7) : url;
+        url = dropProtocol(url);
         int index = url.indexOf('/');
         this.hostName = index != -1 ? url.substring(0, index) : url;
+    }
+
+    private String dropProtocol(String url) {
+        if (url.startsWith("http://"))
+            return url.substring(7);
+        if (url.startsWith("https://"))
+            return url.substring(8);
+        return url;
     }
 
     public static Domain from(String url) {
