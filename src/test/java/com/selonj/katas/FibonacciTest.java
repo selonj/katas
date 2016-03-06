75cd1e8c77fbf3f00c009508167cb9a0cbbdbfca
get-started-tdd <32414154@qq.com>
2016-03-06 23:14:22 +0800

    write the 1st test

---
+++ src/test/java/com/selonj/katas/FibonacciTest.java
@@ -0,0 +1,16 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-3-6.
+ */
+public class FibonacciTest {
+    @Test
+    public void previous2() throws Exception {
+        assertThat(Fib.of(1),equalTo(1));
+    }
+}
