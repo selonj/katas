e1eb5fb62b98b0e9e1e1ed84775bc2397de720bf
get-started-tdd <32414154@qq.com>
2016-03-06 23:22:02 +0800

    write other tests and they were passed

--- src/test/java/com/selonj/katas/FibonacciTest.java
+++ src/test/java/com/selonj/katas/FibonacciTest.java
@@ -11,13 +11,15 @@ import static org.junit.Assert.assertThat;
 public class FibonacciTest {
     @Test
     public void previous2() throws Exception {
-        assertThat(Fib.of(1),equalTo(1));
-        assertThat(Fib.of(2),equalTo(1));
+        assertThat(Fib.of(1), equalTo(1));
+        assertThat(Fib.of(2), equalTo(1));
     }
 
     @Test
     public void fibN() throws Exception {
-        assertThat(Fib.of(3),equalTo(2));
-        assertThat(Fib.of(4),equalTo(3));
+        assertThat(Fib.of(3), equalTo(2));
+        assertThat(Fib.of(4), equalTo(3));
+        assertThat(Fib.of(5), equalTo(5));
+        assertThat(Fib.of(6), equalTo(8));
     }
 }
