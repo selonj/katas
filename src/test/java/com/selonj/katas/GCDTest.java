c2daa0fc47edbc2202315f8fc23354730417da84
get-started-tdd <32414154@qq.com>
2016-03-08 21:14:27 +0800

    write the 3rd test

--- src/test/java/com/selonj/katas/GCDTest.java
+++ src/test/java/com/selonj/katas/GCDTest.java
@@ -12,7 +12,8 @@ public class GCDTest {
 
     @Test
     public void GCDBetweenTwoNumbers() throws Exception {
-        assertThat(NMath.gcd(1,2),equalTo(1));
-        assertThat(NMath.gcd(2,4),equalTo(2));
+        assertThat(NMath.gcd(1, 2), equalTo(1));
+        assertThat(NMath.gcd(2, 4), equalTo(2));
+        assertThat(NMath.gcd(2, 1), equalTo(1));
     }
 }
