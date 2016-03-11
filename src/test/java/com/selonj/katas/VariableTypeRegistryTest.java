e65fc3405e07e82e2c7c02a34e6ab7fec72ba9fc
get-started-tdd <32414154@qq.com>
2016-03-11 23:30:32 +0800

    write 1st unit test

---
+++ src/test/java/com/selonj/katas/VariableTypeRegistryTest.java
@@ -0,0 +1,18 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-3-11.
+ */
+public class VariableTypeRegistryTest {
+    private TypeRegistry registry = new TypeRegistry();
+
+    @Test
+    public void lookupNullAsStringClass() throws Exception {
+        assertThat(registry.lookup(null),equalTo(String.class));
+    }
+}
