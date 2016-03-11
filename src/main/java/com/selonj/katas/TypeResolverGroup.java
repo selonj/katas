d76e98ddab3b61018738688b74852f01c8be6b71
get-started-tdd <32414154@qq.com>
2016-03-12 00:38:26 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/TypeResolverGroup.java
@@ -0,0 +1,11 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-3-12.
+ */
+public class TypeResolverGroup implements TypeResolver{
+    @Override
+    public Class lookup(String type) throws UnresolvedTypeException {
+        return null;
+    }
+}
