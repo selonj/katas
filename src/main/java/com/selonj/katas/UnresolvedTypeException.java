d3eeda63a70227dcb17351980c98118cc97c9974
get-started-tdd <32414154@qq.com>
2016-03-11 23:49:10 +0800

    make the test pass by add message

--- src/main/java/com/selonj/katas/UnresolvedTypeException.java
+++ src/main/java/com/selonj/katas/UnresolvedTypeException.java
@@ -4,4 +4,7 @@ package com.selonj.katas;
  * Created by L.x on 16-3-11.
  */
 public class UnresolvedTypeException extends RuntimeException {
+    public UnresolvedTypeException(String message) {
+        super(message);
+    }
 }
