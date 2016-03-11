7841e1db776eb34d960956c0916b28781307366a
get-started-tdd <32414154@qq.com>
2016-03-12 00:21:37 +0800

    wrhte the 3rd test =>raising exception within unknown alias

--- src/test/java/com/selonj/katas/AliasTypeRegistryTest.java
+++ src/test/java/com/selonj/katas/AliasTypeRegistryTest.java
@@ -6,22 +6,35 @@ import java.math.BigInteger;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 /**
  * Created by L.x on 16-3-12.
  */
 public class AliasTypeRegistryTest {
+
+    private final AliasTypeRegistry registry = new AliasTypeRegistry();
+
     @Test
     public void lookupByAliasWithinCustomType() throws Exception {
-        AliasTypeRegistry registry = new AliasTypeRegistry();
         registry.alias(Object.class, "object");
-        assertThat(registry.lookup("object"),equalTo((Class) Object.class));
+        assertThat(registry.lookup("object"), equalTo((Class) Object.class));
     }
 
     @Test
     public void lookupByAliasWithinCustomType2() throws Exception {
         AliasTypeRegistry registry = new AliasTypeRegistry();
         registry.alias(BigInteger.class, "bigint");
-        assertThat(registry.lookup("bigint"),equalTo((Class) BigInteger.class));
+        assertThat(registry.lookup("bigint"), equalTo((Class) BigInteger.class));
+    }
+
+    @Test
+    public void throwsExceptionIfTypeCannotResolved() throws Exception {
+        try {
+            registry.lookup("unknown");
+            fail("should raising exception");
+        } catch (UnresolvedTypeException expected) {
+            assertThat(expected.getMessage(), equalTo("alias: unknown"));
+        }
     }
 }
