107a76e6b4d8be3cabb48e5add435034b39b7919
get-started-tdd <32414154@qq.com>
2016-03-12 02:58:16 +0800

    replace magic numbers with meaningful name

--- src/main/java/com/selonj/katas/vp/VariableParser.java
+++ src/main/java/com/selonj/katas/vp/VariableParser.java
@@ -7,6 +7,10 @@ import java.util.regex.Pattern;
  * Created by L.x on 16-3-12.
  */
 public class VariableParser {
+    private static final Pattern VARIABLE_REGEX = Pattern.compile("(.*?)(?:\\:(.*?))?(?:\\?\\:(.*?))?");
+    private static final int NAME_GROUP = 1;
+    private static final int TYPE_GROUP = 2;
+    private static final int DEFAULT_VALUE_GROUP = 3;
     private TypeResolver typeResolver;
 
     public VariableParser(TypeResolver typeResolver) {
@@ -15,12 +19,12 @@ public class VariableParser {
 
     public Variable parse(String expression) {
         //todo:trim name,type and defaultValue
-        Matcher matcher = Pattern.compile("(.*?)(?:\\:(.*?))?(?:\\?\\:(.*?))?").matcher(expression);
+        Matcher matcher = VARIABLE_REGEX.matcher(expression);
         //todo:throw VariableParseException if can't matching
         matcher.matches();
-        String name = matcher.group(1);
-        String typeName = matcher.group(2);
-        String defaultValue = matcher.group(3);
+        String name = matcher.group(NAME_GROUP);
+        String typeName = matcher.group(TYPE_GROUP);
+        String defaultValue = matcher.group(DEFAULT_VALUE_GROUP);
         return new Variable(name, typeResolver.lookup(typeName),defaultValue);
     }
 }
