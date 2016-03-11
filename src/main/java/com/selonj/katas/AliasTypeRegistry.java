25102ecde97d12ac4a0b8207269bf5878a92130a
get-started-tdd <32414154@qq.com>
2016-03-12 00:17:17 +0800

    make the test pass by using a map store alias and it type

--- src/main/java/com/selonj/katas/AliasTypeRegistry.java
+++ src/main/java/com/selonj/katas/AliasTypeRegistry.java
@@ -1,14 +1,20 @@
 package com.selonj.katas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by L.x on 16-3-12.
  */
 public class AliasTypeRegistry implements TypeResolver {
+    private Map<String, Class> registeredTypes = new HashMap<>();
+
     @Override
     public Class lookup(String type) {
-        return Object.class;
+        return registeredTypes.get(type);
     }
 
     public void alias(Class type, String alias) {
+        registeredTypes.put(alias, type);
     }
 }
--- src/main/java/com/selonj/katas/TypeResolver.java
+++ src/main/java/com/selonj/katas/TypeResolver.java
@@ -4,5 +4,5 @@ package com.selonj.katas;
  * Created by L.x on 16-3-12.
  */
 public interface TypeResolver {
-    Class lookup(String type);
+    Class lookup(String type) throws UnresolvedTypeException;
 }
