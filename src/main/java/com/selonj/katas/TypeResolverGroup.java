5524b073d7d2cfc78f08a44c70029eb1c49dcf96
get-started-tdd <32414154@qq.com>
2016-03-12 00:46:30 +0800

    make the test pass by lookup in added registry

--- src/main/java/com/selonj/katas/TypeResolverGroup.java
+++ src/main/java/com/selonj/katas/TypeResolverGroup.java
@@ -3,9 +3,15 @@ package com.selonj.katas;
 /**
  * Created by L.x on 16-3-12.
  */
-public class TypeResolverGroup implements TypeResolver{
+public class TypeResolverGroup implements TypeResolver {
+    private TypeResolver resolver;
+
     @Override
     public Class lookup(String type) throws UnresolvedTypeException {
-        return null;
+        return resolver.lookup(type);
+    }
+
+    public void add(TypeResolver resolver) {
+        this.resolver = resolver;
     }
 }
--- src/test/java/com/selonj/katas/TypeResolverGroupTest.java
+++ src/test/java/com/selonj/katas/TypeResolverGroupTest.java
@@ -1,17 +1,29 @@
 package com.selonj.katas;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 
 /**
  * Created by L.x on 16-3-12.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TypeResolverGroupTest {
+    @Mock
+    private TypeResolver registry1;
+
     @Test
     public void lookup() throws Exception {
+        given(registry1.lookup("string")).willReturn(String.class);
+
         TypeResolverGroup group = new TypeResolverGroup();
+        group.add(registry1);
+
         assertThat(group.lookup("string"), equalTo((Class) String.class));
     }
 }
