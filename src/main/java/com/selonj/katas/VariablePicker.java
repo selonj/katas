a62fc81766c8afcf730dbe9c78dd15e00385656b
get-started-tdd <32414154@qq.com>
2016-03-11 22:49:22 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/VariablePicker.java
@@ -0,0 +1,12 @@
+package com.selonj.katas;
+
+import java.util.Set;
+
+/**
+ * Created by L.x on 16-3-11.
+ */
+public class VariablePicker {
+    public Set<Variable> pick(String source) {
+        return null;
+    }
+}
