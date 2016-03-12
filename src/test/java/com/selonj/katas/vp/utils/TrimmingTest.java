33fac0ef5ef3eb84b2fe667ccc1e6f56b0c0d547
get-started-tdd <32414154@qq.com>
2016-03-12 18:50:29 +0800

    write the 3rd unit test of Trimming

--- src/test/java/com/selonj/katas/vp/utils/TrimmingTest.java
+++ src/test/java/com/selonj/katas/vp/utils/TrimmingTest.java
@@ -3,6 +3,7 @@ package com.selonj.katas.vp.utils;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -11,7 +12,12 @@ import static org.junit.Assert.assertThat;
 public class TrimmingTest {
     @Test
     public void trimString() throws Exception {
-        assertThat(Trimming.on(" foo "),equalTo("foo"));
-        assertThat(Trimming.on(" bar "),equalTo("bar"));
+        assertThat(Trimming.on(" foo "), equalTo("foo"));
+        assertThat(Trimming.on(" bar "), equalTo("bar"));
+    }
+
+    @Test
+    public void trimNull() throws Exception {
+        assertThat(Trimming.on(null), nullValue());
     }
 }
