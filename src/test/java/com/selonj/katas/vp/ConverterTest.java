0700135ee65b5d0fcc2b69941f34a225708b9545
get-started-tdd <32414154@qq.com>
2016-03-12 17:27:56 +0800

    write the 3rd unit test of Converter

--- src/test/java/com/selonj/katas/vp/ConverterTest.java
+++ src/test/java/com/selonj/katas/vp/ConverterTest.java
@@ -8,6 +8,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by L.x on 16-3-12.
@@ -15,7 +16,10 @@ import static org.mockito.BDDMockito.given;
 @RunWith(MockitoJUnitRunner.class)
 public class ConverterTest {
     @Mock
-    private Marshaller marshaller;
+    private Marshaller<Time> marshaller;
+    @Mock
+    private Marshaller<String> marshaller2;
+
     private Converter converter = new Converter();
 
     @Test
@@ -25,5 +29,16 @@ public class ConverterTest {
         converter.register(Time.class, marshaller);
 
         assertThat(converter.convert("03:25", Time.class), equalTo(Time.at(3, 25)));
+    }
+
+    @Test
+    public void convertStringToTargetTypeWithinCorrespondingMarshaller() throws Exception {
+        converter.register(Time.class, marshaller);
+        converter.register(String.class, marshaller2);
+
+        converter.convert("03:25", Time.class);
+
+        verifyZeroInteractions(marshaller2);
+        verify(marshaller, only()).marshall("03:25");
     }
 }
