646a41d25864e7faa5037079e942d680d6cda113
get-started-tdd <32414154@qq.com>
2016-03-12 19:00:10 +0800

    let marshallers extends AbstractMarshaller

--- src/main/java/com/selonj/katas/vp/marshallers/StringMarshaller.java
+++ src/main/java/com/selonj/katas/vp/marshallers/StringMarshaller.java
@@ -1,11 +1,11 @@
 package com.selonj.katas.vp.marshallers;
 
-import com.selonj.katas.vp.Marshaller;
+import com.selonj.katas.vp.AbstractMarshaller;
 
 /**
  * Created by L.x on 16-3-12.
  */
-public class StringMarshaller implements Marshaller<String> {
+public class StringMarshaller extends AbstractMarshaller<String> {
     public String marshall(String value) {
         if (value == null) {
             return null;
--- src/main/java/com/selonj/katas/vp/marshallers/TimeMarshaller.java
+++ src/main/java/com/selonj/katas/vp/marshallers/TimeMarshaller.java
@@ -1,5 +1,6 @@
 package com.selonj.katas.vp.marshallers;
 
+import com.selonj.katas.vp.AbstractMarshaller;
 import com.selonj.katas.vp.Marshaller;
 import com.selonj.katas.vp.Time;
 
@@ -8,7 +9,7 @@ import static java.lang.Integer.parseInt;
 /**
  * Created by L.x on 16-3-12.
  */
-public class TimeMarshaller implements Marshaller<Time> {
+public class TimeMarshaller extends AbstractMarshaller<Time> {
 
     private static final int HOURS_POS = 0;
     private static final int MINUTES_POS = 1;
