7b647468c570b191bb4dffca0480078889aa0648
get-started-tdd <32414154@qq.com>
2016-03-12 17:08:58 +0800

    write the 2nd unit test of Converter

--- src/main/java/com/selonj/katas/vp/Converter.java
+++ src/main/java/com/selonj/katas/vp/Converter.java
@@ -5,6 +5,7 @@ package com.selonj.katas.vp;
  */
 //todo: can convert subtypes
 public class Converter {
+    //todo: convert null return null directly?
     public <T> T convert(String value, Class<T> targetType) {
         return (T) value;
     }
--- src/test/java/com/selonj/katas/vp/ConverterTest.java
+++ src/test/java/com/selonj/katas/vp/ConverterTest.java
@@ -9,9 +9,15 @@ import static org.junit.Assert.assertThat;
  * Created by L.x on 16-3-12.
  */
 public class ConverterTest {
-    private Converter converter=new Converter();
+    private Converter converter = new Converter();
+
     @Test
     public void convertToString() throws Exception {
         assertThat(converter.convert("foo", String.class), equalTo("foo"));
+    }
+
+    @Test
+    public void convertToCustomType() throws Exception {
+        assertThat(converter.convert("03:25", Time.class), equalTo(Time.at(2,25)));
     }
 }
