b815a09f7d3bd5f0f09cc8b317169a6900c6dfe5
get-started-tdd <32414154@qq.com>
2016-03-21 22:05:55 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/WordWrap.java
@@ -0,0 +1,14 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-3-21.
+ */
+public class WordWrap {
+    public static WordWrap of(int maxColumns) {
+        return null;
+    }
+
+    public String wrap(String source) {
+        return null;
+    }
+}
