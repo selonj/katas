801f4501610494d7bc42a3360e3ad50e75c1e831
get-started-tdd <32414154@qq.com>
2016-03-21 22:17:34 +0800

    make the test pass by replace white space with line break

--- src/main/java/com/selonj/katas/WordWrap.java
+++ src/main/java/com/selonj/katas/WordWrap.java
@@ -4,11 +4,21 @@ package com.selonj.katas;
  * Created by L.x on 16-3-21.
  */
 public class WordWrap {
+    private static final char LINE_BREAK = '\n';
+    private int maxColumns;
+
+    public WordWrap(int maxColumns) {
+        this.maxColumns = maxColumns;
+    }
+
     public static WordWrap of(int maxColumns) {
-        return new WordWrap();
+        return new WordWrap(maxColumns);
     }
 
     public String wrap(String source) {
+        if (source.length() > maxColumns) {
+            return source.substring(0, maxColumns - 1) + LINE_BREAK + source.substring(maxColumns);
+        }
         return source;
     }
 }
