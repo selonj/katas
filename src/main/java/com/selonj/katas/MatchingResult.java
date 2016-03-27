b15c5cd9b755b3780cff63cebcc0ecae87fe1869
get-started-tdd <32414154@qq.com>
2016-03-27 15:23:54 +0800

    make the test compile

---
+++ src/main/java/com/selonj/katas/MatchingResult.java
@@ -0,0 +1,9 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-3-27.
+ */
+public class MatchingResult {
+    public int bulls;
+    public int cows;
+}
---
+++ src/main/java/com/selonj/katas/SecretNumber.java
@@ -0,0 +1,14 @@
+package com.selonj.katas;
+
+/**
+ * Created by L.x on 16-3-27.
+ */
+public class SecretNumber {
+    public static SecretNumber of(String digits) {
+        return null;
+    }
+
+    public MatchingResult matching(String digits) {
+        return null;
+    }
+}
