f9b413e76097c1769b8124e9fe55c2d0daf860aa
get-started-tdd <32414154@qq.com>
2016-04-04 14:29:55 +0800

    add first acceptance test

---
+++ src/test/java/com/selonj/katas/ArrayMappingTest.java
@@ -0,0 +1,17 @@
+package com.selonj.katas;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by L.x on 16-4-4.
+ */
+public class ArrayMappingTest {
+    @Test
+    public void clonesAnArrayWhenMapsAnArrayWithNoMapping() throws Exception {
+        Array<Integer> result = Array.of(1, 2, 3).map(withNoMapping());
+        assertThat(result.toArray(), equalTo(new int[]{1, 2, 3}));
+    }
+}
