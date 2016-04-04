07ede75ad3bfeb812978682511265280ae06b91d
get-started-tdd <32414154@qq.com>
2016-04-04 15:05:25 +0800

    add the 4th acceptance test

--- src/main/java/com/selonj/katas/Array.java
+++ src/main/java/com/selonj/katas/Array.java
@@ -49,4 +49,6 @@ public class Array<T> {
     public String toString() {
         return Arrays.toString(items);
     }
+
+
 }
--- src/test/java/com/selonj/katas/ArrayMappingTest.java
+++ src/test/java/com/selonj/katas/ArrayMappingTest.java
@@ -21,6 +21,17 @@ public class ArrayMappingTest {
         assertThat(result, equalTo(Array.of("1", "2", "3")));
     }
 
+    @Test
+    public void cannotEffectedOriginalArrayWhenMappedArrayChanged() throws Exception {
+        Array<Integer> original = Array.of(1, 2, 3);
+        Array<Integer> result = original.map(withNoMapping());
+
+        result.set(0, 50);
+
+        assertThat(result.get(0), equalTo(50));
+        assertThat(original.get(0), equalTo(1));
+    }
+
     private Transformation<Integer, String> stringify() {
         return new Transformation<Integer, String>() {
             @Override
